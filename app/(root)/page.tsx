9e0989e2a361c2fbe14fa43f14de7716a0a7fc9c
Patrik <patriklevak@gmail.com>
2024-03-07 19:39:47 +0100

    implement homepage, pagination and search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,52 @@
-export default function Home() {
-  return <div className="">Home</div>;
+import { navLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { Collection } from "@/components/shared/Collection";
+import { getAllImages } from "@/lib/actions/image.actions";
+
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({ page, searchQuery });
+
+  return (
+    <>
+      <section className="home">
+        <h1 className="home-heading">
+          Discover The World of AI Image Transformation with Zap.ai
+        </h1>
+
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li>
+                <Image
+                  src={link.icon}
+                  alt="image"
+                  width={24}
+                  height={24}
+                  className="flex-center rounded-full w-fit bg-white p-4"
+                />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
+    </>
+  );
 }
